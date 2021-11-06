6b4f854caf8636446b59f8d58c98069b1b047614
Anastasia Shmeleva <anastasiashmeleva@Anastasias-MacBook-Air-2.local>
2021-11-06 11:45:21 +0300

    array map fix

--- src/Stars.js
+++ src/Stars.js
@@ -2,20 +2,15 @@ import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import Star from './Star';
 
-export default function Stars(count) {
-    let rating = [];
+export default function Stars(props) {
 
-    if (count.count > 5 || count.count < 1) {
-        return <ul className="card-body-stars u-clearfix"></ul>
-    }
-
-    for (let i = 1; i <= count.count; i++) {
-        rating.push(i);
+    if (props.count > 5 || props.count < 1) {
+        return null;
     }
 
     return (
         <ul className="card-body-stars u-clearfix">
-            {rating.map(() => {
+            {[...Array(props.count).keys()].map(() => {
                 return <Star key={shortid.generate()}></Star>
             })}
         </ul>
